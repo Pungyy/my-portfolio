6165b07dd229e00dcc4110525108fb5f95265ca7
ANIL Ibrahim <ianil@one-system.fr>
2025-04-21 20:11:56 +0200

    correction des conflits

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,7 @@
 "use client";
-import { useRef, useEffect, useState } from "react";
-import ReactLenis from "@studio-freight/react-lenis";
+
+import { useRef, useState, useEffect } from "react";
+import Lenis from "lenis"; // Import de Lenis avec le bon package
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import SplitType from "split-type";
@@ -8,13 +9,39 @@ import emailjs from "emailjs-com";
 
 gsap.registerPlugin(useGSAP);
 
-const Info = () => {
+export default function Contact() {
   const container = useRef();
+  const formRef = useRef();
   const scrollIconRef = useRef();
-  const [isAtBottom, setIsAtBottom] = useState(false);
   const [statusMessage, setStatusMessage] = useState("");
-  
-  const formRef = useRef(); // Reference to the form element
+  const [isAtBottom, setIsAtBottom] = useState(false);
+
+  const lenisRef = useRef(null); // Référence à Lenis
+
+  useEffect(() => {
+    // Initialisation de Lenis pour le défilement fluide
+    lenisRef.current = new Lenis({
+      duration: 1.2, // Durée du défilement
+      easing: (t) => t, // Fonction d'easing (facultatif)
+      smoothWheel: true, // Défilement fluide avec la molette
+      smoothTouch: true, // Défilement fluide sur mobile
+    });
+
+    // Fonction de rafraîchissement
+    function raf(time) {
+      lenisRef.current.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    // Demander une nouvelle frame pour Lenis
+    requestAnimationFrame(raf);
+
+    return () => {
+      if (lenisRef.current) {
+        lenisRef.current.destroy(); // Détruire Lenis au démontage
+      }
+    };
+  }, []);
 
   // Scroll listener to detect when the user reaches the bottom of the page
   useEffect(() => {
@@ -66,6 +93,39 @@ const Info = () => {
     }
   }, [isAtBottom]);
 
+  // Send email function using EmailJS
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // Pass the form reference as the third parameter
+    emailjs
+      .sendForm(
+        "service_x2l1ilq",
+        "template_y3he5x9",
+        formRef.current,
+        "si5fFBZ6O48KSc0v3"
+      )
+      .then(
+        (result) => {
+          setStatusMessage("Message sent successfully!");
+          formRef.current.reset();
+          console.log(result);
+        },
+        (error) => {
+          setStatusMessage("Error sending message. Please try again.");
+          console.log("Error details:", error);
+        }
+      );
+  };
+
+  // Function to scroll to the bottom of the page
+  const scrollToBottom = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
   // GSAP animations for text
   useGSAP(
     () => {
@@ -100,64 +160,28 @@ const Info = () => {
     { scope: container }
   );
 
-  // Send email function using EmailJS
-  const sendEmail = (e) => {
-    e.preventDefault();
-
-    // Pass the form reference as the third parameter
-    emailjs
-      .sendForm(
-        "service_x2l1ilq",
-        "template_y3he5x9",
-        formRef.current,
-        "si5fFBZ6O48KSc0v3"
-      )
-      .then(
-        (result) => {
-          setStatusMessage("Message sent successfully!");
-          formRef.current.reset();
-          console.log(result);
-        },
-        (error) => {
-          setStatusMessage("Error sending message. Please try again.");
-          console.log("Error details:", error);
-        }
-      );
-  };
-
-  // Function to scroll to the bottom of the page
-  const scrollToBottom = () => {
-    window.scrollTo({
-      top: document.documentElement.scrollHeight,
-      behavior: "smooth",
-    });
-  };
-
   return (
-    <ReactLenis root>
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        rel="stylesheet"
-      />
+    <div className="info flex flex-col min-h-screen px-4" ref={container}>
+      {/* Première section - Image et texte en haut (prend 100vh) */}
+      <div className="flex flex-col md:flex-row items-center justify-around w-full h-screen">
+        <div className="flex justify-center mt-10 md:mt-0 md:w-1/2">
+          <img
+            src="/pungy.png"
+            alt="Profile Picture"
+            className="w-[90%] h-auto md:w-[80%] rounded-full"
+          />
+        </div>
 
-      <div className="info flex items-center justify-center min-h-screen px-4" ref={container}>
-        <div className="flex flex-col md:flex-row items-center md:items-center justify-around w-full">
-          {/* Image */}
-          <div className="flex justify-center mt-10 md:mt-0 md:w-1/2">
-            <img src="/pungy.png" alt="Profile Picture" className="w-[90%] h-auto md:w-[80%] rounded-full" />
-          </div>
-
-          {/* Texte (aligné au centre verticalement) */}
-          <div className="text-center text-sm sm:text-xl md:text-2xl px-4 md:px-0 md:w-1/2 flex items-center">
-            <p>
-              Hello! I'm Ibrahim, a Full-Stack Developer with experience in frontend technologies like HTML, CSS, and JavaScript.
-              I've worked as a Web Developer at Novances IT and a Full-Stack Developer at One System. Currently,
-              I'm advancing my skills at My Digital School Lyon to become a stronger web developer.
-            </p>
-          </div>
+        <div className="text-center text-sm sm:text-xl md:text-2xl px-4 md:px-0 md:w-1/2 flex items-center">
+          <p>
+            Hello! I'm Ibrahim, a Full-Stack Developer with experience in frontend technologies like HTML, CSS, and JavaScript.
+            I've worked as a Web Developer at Novances IT and a Full-Stack Developer at One System. Currently,
+            I'm advancing my skills at My Digital School Lyon to become a stronger web developer.
+          </p>
         </div>
       </div>
 
+      {/* Deuxième section - Formulaire en bas (prend 100vh) */}
       <section className="contact-section bg-black text-white py-16 flex items-center justify-center h-screen">
         <div className="max-w-5xl w-full mx-auto px-4">
           <h2 className="text-4xl font-semibold mb-8 text-center">CONTACT ME</h2>
@@ -167,7 +191,7 @@ const Info = () => {
               <input
                 type="text"
                 id="name"
-                name="from_name" // Correspond to {{from_name}} in the template
+                name="from_name"
                 className="w-full p-4 border-2 border-white bg-black text-white placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="ENTER YOUR NAME"
                 required
@@ -178,7 +202,7 @@ const Info = () => {
               <input
                 type="email"
                 id="email"
-                name="from_email" // Correspond to {{from_email}} in the template
+                name="from_email"
                 className="w-full p-4 border-2 border-white bg-black text-white placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="ENTER YOUR EMAIL"
                 required
@@ -188,7 +212,7 @@ const Info = () => {
               <label htmlFor="message" className="block text-lg font-medium mb-2">YOUR MESSAGE</label>
               <textarea
                 id="message"
-                name="message" // Correspond to {{message}} in the template
+                name="message"
                 rows="6"
                 className="w-full p-4 border-2 border-white bg-black text-white placeholder-gray-400 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="WRITE YOUR MESSAGE"
@@ -212,6 +236,7 @@ const Info = () => {
         </div>
       </section>
 
+      {/* Scroll icon */}
       <div
         ref={scrollIconRef}
         onClick={scrollToBottom}
@@ -219,8 +244,6 @@ const Info = () => {
       >
         <i className="fas fa-chevron-down"></i>
       </div>
-    </ReactLenis>
+    </div>
   );
-};
-
-export default Info;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 "use client";
 
 import { useRef, useState, useEffect } from "react";
-import ReactLenis from "@studio-freight/react-lenis";
+// Remplacer ReactLenis par Lenis
+import Lenis from "lenis"; 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import SplitType from "split-type";
@@ -29,7 +30,18 @@ export default function Home() {
   const [showScrollIcon, setShowScrollIcon] = useState(false);
 
   useEffect(() => {
-    
+    // Créer une instance de Lenis
+    const lenis = new Lenis();
+
+    // Fonction de rafraîchissement
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    // Démarrer l'animation de défilement
+    requestAnimationFrame(raf);
+
     const newNeonStyles = Array.from({ length: 12 }).map(() => ({
       top: `${Math.random() * 75 + 5}%`,
       left: `${Math.random() * 75 + 5}%`,
@@ -42,10 +54,10 @@ export default function Home() {
 
     // Animation du texte "Ibrahim"
     const heroText = new SplitType(".hero-section h1", { types: "chars" });
-    gsap.set(heroText.chars, { y: 400, opacity: 0 }); // Définit l'opacité initiale à 0
+    gsap.set(heroText.chars, { y: 400, opacity: 0 });
     gsap.to(heroText.chars, {
       y: 0,
-      opacity: 1, // Transition de l'opacité de 0 à 1
+      opacity: 1,
       duration: 0.6,
       stagger: 0.05,
       ease: "power4.out",
@@ -70,14 +82,14 @@ export default function Home() {
     gsap.to(roadmapItems, {
       opacity: 1,
       y: 0,
-      duration: 0.8, // Réduit la durée pour une animation plus rapide
-      stagger: 0.15, // Diminue l'intervalle entre les animations
-      ease: "power3.out", // Rend l'animation plus fluide
+      duration: 0.8,
+      stagger: 0.15,
+      ease: "power3.out",
       scrollTrigger: {
         trigger: roadmapRef.current,
-        start: "top 95%", // Déclenche plus tôt sur mobile
-        end: "bottom 50%", // Permet un déroulement plus fluide
-        toggleActions: "play none none none", // Évite une réinitialisation inutile
+        start: "top 95%",
+        end: "bottom 50%",
+        toggleActions: "play none none none",
       },
     });
 
@@ -101,91 +113,75 @@ export default function Home() {
     const handleScroll = () => setScrolling(window.scrollY > 50);
     window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []); // Ne change pas le tableau de dépendances ici
 
   return (
-    <ReactLenis root>
+    <div className="relative text-white overflow-hidden min-h-screen">
       <Navbar />
       <GlowCursor />
       <MoonScene />
 
-      <div className="relative text-white overflow-hidden min-h-screen">
-        {/* Effet néon */}
-        {/* <div className="absolute inset-0 pointer-events-none">
-          {neonStyles.map((style, index) => (
-            <div
-              key={index}
-              className={`absolute blur-2xl opacity-30 animate-pulse ${["bg-blue-500", "bg-pink-500", "bg-green-500", "bg-teal-400", "bg-purple-400", "bg-yellow-400", "bg-red-500"][index % 7]}`}
-              style={style}
-            ></div>
-          ))}
-        </div> */}
-
-        {/* Section Hero */}
-        <div className="hero-section h-screen flex flex-col items-center justify-center relative z-10" ref={container}>
-          <h1 className="text-6xl md:text-9xl font-extrabold tracking-wide text-center">Ibrahim</h1>
-          <p className="text-1xl md:text-3xl mt-4">Full-Stack Developer | Web Enthusiast</p>
-        </div>
-
-        {/* Section Roadmap pour les grands écrans */}
-        <div className="relative text-white min-h-screen p-10 hidden md:block" ref={roadmapRef}>
-          <h2 className="text-4xl font-bold text-center mb-10">My Career Path</h2>
-          <div className="relative flex flex-col items-center">
-            <div className="w-1 bg-gray-500 absolute h-full left-1/2 transform -translate-x-1/2"></div>
-            {roadmapData.map((item, index) => (
-              <div key={index} className="roadmap-item flex flex-col items-center mb-10 w-full max-w-3xl relative">
-                <div className={`${index % 2 === 0 ? "mr-auto text-right" : "ml-auto text-left"} p-4 rounded-lg w-64 shadow-lg`}>
-                  <p className="text-sm text-gray-400">{item.year}</p>
-                  <h3 className="text-lg font-bold">{item.title}</h3>
-                  <p className="text-sm">{item.company}</p>
-                </div>
-                <div className="w-4 h-4 bg-white rounded-full absolute left-1/2 transform -translate-x-1/2"></div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Section Roadmap en version mobile */}
-        <div className="relative text-white min-h-screen p-10 md:hidden">
-          <h2 className="text-4xl font-bold text-center mb-10">My Career Path</h2>
-          <div className="relative flex flex-col items-center">
-            {roadmapData.map((item, index) => (
-              <div key={index} className="roadmap-item flex flex-col items-center text-center mb-10 w-full max-w-3xl relative">
-                <div className="p-4 rounded-lg w-full shadow-lg mb-4">
-                  <p className="text-sm text-white">{item.year}</p>
-                  <h3 className="text-lg font-bold">{item.title}</h3>
-                  <p className="text-sm">{item.company}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Section Skills */}
-        <div className="skills-section h-screen flex flex-col items-center justify-center relative z-10" ref={skillsRef}>
-          <h2 className="text-4xl font-bold mb-6">Technical Skills</h2>
-          <div className="flex flex-wrap justify-center gap-6 px-4 md:px-0">
-            {[{ icon: "fab fa-html5 text-orange-500", label: "HTML" }, { icon: "fab fa-css3-alt text-blue-500", label: "CSS" }, { icon: "fab fa-js-square text-yellow-500", label: "JavaScript" }, { icon: "fab fa-react text-cyan-400", label: "React" }, { icon: "fab fa-node-js text-green-500", label: "Node.js (Express)" }, { icon: "fab fa-php text-purple-600", label: "PHP" }, { icon: "fab fa-bootstrap text-purple-500", label: "Bootstrap" }, { icon: <SiTailwindcss className="text-blue-400 mr-2.5" />, label: "Tailwind" }].map((skill, index) => (
-              <div key={index} className="skills-item text-xl flex items-center p-4 w-1/2 sm:w-1/4 md:w-1/4 justify-center">
-                {typeof skill.icon === "string" ? <i className={`${skill.icon} text-4xl mr-2.5`}></i> : skill.icon}
-                {skill.label}
-              </div>
-            ))}
-          </div>
-        </div>
-
-        {/* Icône de scroll */}
-        {showScrollIcon && !scrolling && (
-          <div
-            ref={scrollIconRef}
-            onClick={() => window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" })}
-            className="fixed bottom-20 left-1/2 transform -translate-x-1/2 text-white text-3xl w-16 h-16 flex items-center justify-center border-4 border-white rounded-full cursor-pointer"
-          >
-            <i className="fas fa-chevron-down"></i>
-          </div>
-        )}
+      <div className="hero-section h-screen flex flex-col items-center justify-center relative z-10" ref={container}>
+        <h1 className="text-6xl md:text-9xl font-extrabold tracking-wide text-center">Ibrahim</h1>
+        <p className="text-1xl md:text-3xl mt-4">Full-Stack Developer | Web Enthusiast</p>
       </div>
-    </ReactLenis>
+
+      <div className="relative text-white min-h-screen p-10 hidden md:block" ref={roadmapRef}>
+        <h2 className="text-4xl font-bold text-center mb-10">My Career Path</h2>
+        <div className="relative flex flex-col items-center">
+          <div className="w-1 bg-gray-500 absolute h-full left-1/2 transform -translate-x-1/2"></div>
+          {roadmapData.map((item, index) => (
+            <div key={index} className="roadmap-item flex flex-col items-center mb-10 w-full max-w-3xl relative">
+              <div className={`${index % 2 === 0 ? "mr-auto text-right" : "ml-auto text-left"} p-4 rounded-lg w-64 shadow-lg`}>
+                <p className="text-sm text-gray-400">{item.year}</p>
+                <h3 className="text-lg font-bold">{item.title}</h3>
+                <p className="text-sm">{item.company}</p>
+              </div>
+              <div className="w-4 h-4 bg-white rounded-full absolute left-1/2 transform -translate-x-1/2"></div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative text-white min-h-screen p-10 md:hidden">
+        <h2 className="text-4xl font-bold text-center mb-10">My Career Path</h2>
+        <div className="relative flex flex-col items-center">
+          {roadmapData.map((item, index) => (
+            <div key={index} className="roadmap-item flex flex-col items-center text-center mb-10 w-full max-w-3xl relative">
+              <div className="p-4 rounded-lg w-full shadow-lg mb-4">
+                <p className="text-sm text-white">{item.year}</p>
+                <h3 className="text-lg font-bold">{item.title}</h3>
+                <p className="text-sm">{item.company}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="skills-section h-screen flex flex-col items-center justify-center relative z-10" ref={skillsRef}>
+        <h2 className="text-4xl font-bold mb-6">Technical Skills</h2>
+        <div className="flex flex-wrap justify-center gap-6 px-4 md:px-0">
+          {[{ icon: "fab fa-html5 text-orange-500", label: "HTML" }, { icon: "fab fa-css3-alt text-blue-500", label: "CSS" }, { icon: "fab fa-js-square text-yellow-500", label: "JavaScript" }, { icon: "fab fa-react text-cyan-400", label: "React" }, { icon: "fab fa-node-js text-green-500", label: "Node.js (Express)" }, { icon: "fab fa-php text-purple-600", label: "PHP" }, { icon: "fab fa-bootstrap text-purple-500", label: "Bootstrap" }, { icon: <SiTailwindcss className="text-blue-400 mr-2.5" />, label: "Tailwind" }].map((skill, index) => (
+            <div key={index} className="skills-item text-xl flex items-center p-4 w-1/2 sm:w-1/4 md:w-1/4 justify-center">
+              {typeof skill.icon === "string" ? <i className={`${skill.icon} text-4xl mr-2.5`}></i> : skill.icon}
+              {skill.label}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {showScrollIcon && !scrolling && (
+        <div
+          ref={scrollIconRef}
+          onClick={() => window.scrollTo({ top: document.documentElement.scrollHeight, behavior: "smooth" })}
+          className="fixed bottom-20 left-1/2 transform -translate-x-1/2 text-white text-3xl w-16 h-16 flex items-center justify-center border-4 border-white rounded-full cursor-pointer"
+        >
+          <i className="fas fa-chevron-down"></i>
+        </div>
+      )}
+    </div>
   );
 }
